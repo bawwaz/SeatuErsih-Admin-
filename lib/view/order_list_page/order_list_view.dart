156a33caf97ab0dd36b926b75174d3116b4259c5
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-06-02 22:35:39 +0700

    style: [DNA] extract widget

--- lib/view/order_list_page/order_list_view.dart
+++ lib/view/order_list_page/order_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:seatu_ersih_admin/widget/order_list_page/item_card.dart';
+import 'package:seatu_ersih_admin/widget/order_list_page/item_card2.dart';
 
 class OrderListView extends StatelessWidget {
   const OrderListView({super.key});
@@ -48,72 +50,7 @@ class OrderListView extends StatelessWidget {
             SizedBox(
               height: 15,
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  children: [
-                    Card(
-                      elevation: 4,
-                      color: Colors.white,
-                      child: Container(
-                        width: 80,
-                        height: 80,
-                        child: Image.asset(
-                          'assets/img/shoes-icon2.png',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 5),
-                              child: Text(
-                                'Regular Clean',
-                                style: GoogleFonts.poppins(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                  color: Color(0xFF000000),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Icon(
-                            Icons.info_outline,
-                            color: Color(0xFF7EC1EB),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        'x2',
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFF000000),
-                        ),
-                      ),
-                      Text(
-                        '50.000',
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFF7EC1EB),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            ItemCard(),
             Center(
               child: Padding(
                 padding: const EdgeInsets.only(top: 20),
@@ -142,72 +79,7 @@ class OrderListView extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  children: [
-                    Card(
-                      elevation: 4,
-                      color: Colors.white,
-                      child: Container(
-                        width: 80,
-                        height: 80,
-                        child: Image.asset(
-                          'assets/img/shoes-icon2.png',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 5),
-                              child: Text(
-                                'Regular Clean',
-                                style: GoogleFonts.poppins(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                  color: Color(0xFF000000),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Icon(
-                            Icons.info_outline,
-                            color: Color(0xFF7EC1EB),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        'x2',
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFF000000),
-                        ),
-                      ),
-                      Text(
-                        '50.000',
-                        style: GoogleFonts.poppins(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFF7EC1EB),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            ItemCard2(),
           ],
         ),
       ),
