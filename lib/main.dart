20fd2191180f732ce65b25b955616aca66ffcb98
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-06-03 12:14:53 +0700

    [DNA] add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:seatu_ersih_admin/view/edit_banner_page/edit_banner_view.dart';
-import 'package:seatu_ersih_admin/view/edit_card_page/edit_card_view.dart';
-import 'package:seatu_ersih_admin/view/home_page/HomepageView.dart';
-import 'package:seatu_ersih_admin/view/login_page/LoginpageView.dart';
-import 'package:seatu_ersih_admin/view/order_detail_page/order_detail_view.dart';
-import 'package:seatu_ersih_admin/view/order_list_page/order_list_view.dart';
-import 'package:seatu_ersih_admin/view/register_page/RegisterviewPage.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:seatu_ersih_admin/routes/routes.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
@@ -16,60 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.homepage,
+      getPages: Routes.pages,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF7EC1EB)),
         useMaterial3: true,
-      ),
-      home: RegisterPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
