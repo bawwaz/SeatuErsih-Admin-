3b6939a01ae925f5c4ba517ca03c03e3a71b87ac
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-08-05 23:28:09 +0700

    feat: [DNA] setup firebase notif

--- lib/app/global_component/navbar/btmnavcontroller.dart
+++ lib/app/global_component/navbar/btmnavcontroller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 
 class BottomNavigationController extends GetxController {
@@ -5,5 +6,17 @@ class BottomNavigationController extends GetxController {
 
   void changePage(int index) {
     currentIndex.value = index;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    initNotif();
+  }
+
+  Future<void> initNotif() async {
+    await FirebaseMessaging.instance.requestPermission();
+    final token = await FirebaseMessaging.instance.getToken();
+    print("Token Firebase = $token");
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:seatu_ersih_admin/firebase_options.dart';
 import 'app/router/app_pages.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(MyApp());
 }
 
