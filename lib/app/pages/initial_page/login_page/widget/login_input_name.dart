f2f5ca429b91b17980a5628e529b328bd5aa5659
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-08-25 14:55:10 +0700

    style: [DNA] style login screen

--- lib/app/pages/initial_page/login_page/widget/login_input_name.dart
+++ lib/app/pages/initial_page/login_page/widget/login_input_name.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_instance/get_instance.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:seatu_ersih_admin/app/pages/initial_page/login_page/login_controller.dart';
 
 class InputName extends StatelessWidget {
@@ -15,11 +16,11 @@ class InputName extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
         ),
         labelText: "Email",
+        labelStyle: GoogleFonts.poppins(),
         prefixIcon: Icon(
           Icons.person,
           color: Color(0xFF8A8A8A),
         ),
-        
       ),
     );
   }
